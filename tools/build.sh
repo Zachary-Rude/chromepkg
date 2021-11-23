93f0eb55891664866c228ce22ba3d1870ae3d248
Zachary-Rude <84681078+Zachary-Rude@users.noreply.github.com>
2021-11-23 13:45:46 -0800

    Update build.sh

--- tools/build.sh
+++ tools/build.sh
@@ -4,11 +4,11 @@ case $1 in
 *)  keep="";;
 esac
 set -o pipefail
-# yes | crew install buildessential $keep
+# yes | crpkg install buildessential $keep
 arch=`uname -m`
 for i in "$@"; do
-    version=`grep '\<version ['"'"'"]' /usr/local/lib/crew/packages/$i.rb | head -1 | sed -e 's/^.*version *['"'"'"]//' -e 's/['"'"'"].*$//'`
-    yes | crew build $i $keep 2>&1 | tee "$i-$version-chromeos-$arch.log"
+    version=`grep '\<version ['"'"'"]' /usr/local/lib/crpkg/packages/$i.rb | head -1 | sed -e 's/^.*version *['"'"'"]//' -e 's/['"'"'"].*$//'`
+    yes | crpkg build $i $keep 2>&1 | tee "$i-$version-chromeos-$arch.log"
     case .$? in
     .141) ;;        # ignore SIGPIPE
     *)    exit 1;;  # abort at errors
