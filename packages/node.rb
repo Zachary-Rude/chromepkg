1bdd0f07744452bfcb7c01f9ccee8745bdb5adde
Zachary-Rude <zacharyrude10@gmail.com>
2021-11-24 16:12:53 -0800

    Update node.rb

--- packages/node.rb
+++ packages/node.rb
@@ -3,7 +3,7 @@ require 'package'
 class Node < Package
   description 'As an asynchronous event driven JavaScript runtime, Node is designed to build scalable network applications.'
   homepage 'https://nodejs.org/en/'
-  version '8.9.1'
+  version '16.13.0'
   license 'Apache-1.1, Apache-2.0, BSD, BSD-2 and MIT'
   compatibility 'all'
 
