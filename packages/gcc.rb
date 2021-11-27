3368488a29e25a9e514f2a212ea8c7143fb9b00b
Zachary-Rude <84681078+Zachary-Rude@users.noreply.github.com>
2021-11-27 14:54:06 -0800

    Update and rename gcc8.rb to gcc.rb

--- packages/gcc.rb
+++ packages/gcc.rb
@@ -1,6 +1,6 @@
 require 'package'
 
-class Gcc8 < Package
+class Gcc < Package
   description 'The GNU Compiler Collection includes front ends for C, C++, Objective-C, Fortran, Ada, and Go.'
   homepage 'https://www.gnu.org/software/gcc/'
   version '8.3.0'
