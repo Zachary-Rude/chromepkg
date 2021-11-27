6ebe66395b16eef12cdad7463beb0e2e5277be8b
Zachary-Rude <84681078+Zachary-Rude@users.noreply.github.com>
2021-11-27 14:55:13 -0800

    Update const.rb

--- lib/const.rb
+++ lib/const.rb
@@ -1,6 +1,6 @@
 # Defines common constants used in different parts of crpkg
 
-CREW_VERSION = '1.17.5'
+CREW_VERSION = '1.17.6'
 
 ARCH_ACTUAL = `uname -m`.strip
 # This helps with virtualized builds on aarch64 machines
